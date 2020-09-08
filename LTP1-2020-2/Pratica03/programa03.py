875104f3fd75d947d68ee281b0d10254d799b3d3
rafaCDJ <70581467+rafaCDJ@users.noreply.github.com>
2020-09-08 08:56:10 -0300

    Create programa03.py

---
+++ LTP1-2020-2/Pratica03/programa03.py
@@ -0,0 +1,5 @@
+#Pede para o usuario 2 numeros e realiza a soma deles
+numero1 = int(input("digite um numero:"))
+numero2 = int(input("digite outro numero:"))
+result = numero1 + numero2
+print("o resultado da soma é:", result)
