1f6f66708bc69761e0455dd6c557b22a624c8113
rafaCDJ <70581467+rafaCDJ@users.noreply.github.com>
2020-09-08 09:18:27 -0300

    Update programa04.py

--- LTP1-2020-2/Pratica03/programa04.py
+++ LTP1-2020-2/Pratica03/programa04.py
@@ -1,5 +1,5 @@
 #Pede um valor em cm o programa transforma em Metros
-#float é para numeros reais
+#float é para numeros inteiros
 valor_cm = float(input("informe um valor em cm:"))
 valor_m = valor_cm / 100
 print("o valor",valor_cm,"cm","em metros é:",valor_m)
