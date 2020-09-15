87a21b805603c62d4be70101926544d0346a82e7
rafaCDJ <70581467+rafaCDJ@users.noreply.github.com>
2020-09-15 08:22:39 -0300

    Create programa02.py

---
+++ LTP1-2020-2/Pratica04/programa02.py
@@ -0,0 +1,12 @@
+#pede parao usuario digitar uma senha e valida ela com a senha secreta
+senha_secreta = "123456"
+
+#pede para o usuario digitar sua senha
+senha = input("informe sua senha")
+
+#verifica se a senha do usuario esta certa
+if senha == senha_secreta
+ print("bem vindo Hackerman")
+
+if senha != senha_secreta
+ print("acesso negado")
