2fe4de7452570cb663c56f876b34dfd4c3f3758e
rafaCDJ <70581467+rafaCDJ@users.noreply.github.com>
2020-10-31 16:47:26 -0300

    Update piloto.py

--- LTP1-2020-2/projeto/piloto.py
+++ LTP1-2020-2/projeto/piloto.py
@@ -1,17 +1,22 @@
 import random
 repeticao = 0
+email1 = 'gmail'
 senha_id1 = '2468'
 senha_id2 = '123456'
 senha_id3 = '1234'
 saldo_ID1 = 1000
 saldo_ID2 = 250
 saldo_ID3 = 3000
+resposta = 'Sim'
+Sim = True
 usuario = input('Informe o seu ID: ')
-senha = input('Informe a sua senha: ')
 email = input('Informe o seu e-mail: ')
-while repeticao == 0:
+senha = input('Informe a sua senha: ')
+
+while resposta == 'Sim':
  if usuario == 'id1':
-  if senha == senha_id1:
+  if email == 'email1':
+   if senha == senha_id1:
     recebedor = input('Informe o ID recebedor do pagamento: ')
     print('Seu saldo atual é de R$',saldo_ID1)
     valor_pagamento = float(input('Informe o valor a ser pago: '))
@@ -23,6 +28,4 @@ while repeticao == 0:
       print('Valor da transação R$',valor_pagamento)
       print("sua chave de segurança é:"),print(random.randrange(1000, 9999))
       print(email)
-
-else:
-  print('Senha ou usuário incorreto')
+      resposta = input('Deseja realizar outra transação? (Sim ou Não) ')
