15cc7450b412ffb7dc424a91ce4108576cbc24cf
rafaCDJ <70581467+rafaCDJ@users.noreply.github.com>
2020-10-31 15:49:47 -0300

    Update piloto.py

--- LTP1-2020-2/projeto/piloto.py
+++ LTP1-2020-2/projeto/piloto.py
@@ -1,16 +1,19 @@
 import random
-
+id1 = 1
+id2 = 2
+id2 = 3
 senha_id1 = '2468'
 senha_id2 = '123456'
 senha_id3 = '1234'
 saldo_ID1 = 1000
 saldo_ID2 = 250
 saldo_ID3 = 3000
-usuario = input('Informe o seu ID: ')
-senha = input('Informe a sua senha: ')
-email = input('Informe o seu e-mail: ')
-if senha == senha_id1:
+usuario = int(input('Informe o seu ID: '))
+if usuario == 1:
+ senha = int(input('Informe a sua senha: '))
+ if senha == senha_id1:
   recebedor = input('Informe o ID recebedor do pagamento: ')
+  print('Seu saldo atual é de R$',saldo_ID1)
   valor_pagamento = float(input('Informe o valor a ser pago: '))
   if saldo_ID1 > valor_pagamento:
     pagamento = (saldo_ID1-valor_pagamento)
@@ -19,5 +22,6 @@ if senha == senha_id1:
     print('ID recebdor:', recebedor)
     print('Valor da transação R$',valor_pagamento)
     print("sua chave de segurança é:"),print(random.randrange(1000, 9999))
+    print(email)
 else:
-  print('Senha incorreta')
+  print('ususario não encontrado')
